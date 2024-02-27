df438bee6959d12810a07e10704e18565d3d7ce6
maxwell1565 <146258976+maxwell1565@users.noreply.github.com>
2024-02-27 15:30:26 -0600

    finished method overloading exercise

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,54 @@
-﻿namespace MethodOverloading
+﻿
+namespace MethodOverloading
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var a = 10;
+            var b = 12;
+            var answer = Add(a, b);
+
+            decimal x = 10.1m;
+            var y = 12.1m;
+            var decimalNum = Add(x, y);
+
+            var totalAmount = Add(1, 1, true);
+
+            Console.WriteLine($"Interger answer {answer}. Decimal answer {decimalNum}. Amount of money in your bank account is {totalAmount}.");
+
         }
+
+        public static int Add(int a, int b)
+        {
+            return (a + b);
+        }
+        public static decimal Add(decimal a, decimal b)
+        {
+            return (a + b);
+        }
+
+        static string Add(int a, int b, bool dollarAmount)
+        {
+            var sum = a + b;
+            if (dollarAmount == true && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if (dollarAmount == true && sum == 1)
+            {
+                return "That's one dollar";
+            }
+            else if (dollarAmount == true && sum < 1)
+            {
+                return $"{sum} dollars";
+            }
+            else
+            {
+                return sum.ToString();
+            }
+        }
+
     }
+
 }
